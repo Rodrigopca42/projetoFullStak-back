93b86bb8555e4b588d0666a306f7673a06417a26
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-10-16 15:52:15 -0300

    Exceção para objetos não encontrados

--- helpdesk-backend/src/main/java/br/com/bennytech/helpdesk/services/TecnicoService.java
+++ helpdesk-backend/src/main/java/br/com/bennytech/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.bennytech.helpdesk.domain.Tecnico;
 import br.com.bennytech.helpdesk.repositories.TecnicoRepository;
+import br.com.bennytech.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -17,7 +18,7 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id = " +id));
 	}
 	
 	
