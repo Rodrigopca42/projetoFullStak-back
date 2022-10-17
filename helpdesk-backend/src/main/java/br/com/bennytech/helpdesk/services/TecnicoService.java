6bb0e44a8608efb9a33f545ff2f0f0be83c13ac6
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-10-17 10:00:27 -0300

    Update - metodo PUT para Tecnico implementado

--- helpdesk-backend/src/main/java/br/com/bennytech/helpdesk/services/TecnicoService.java
+++ helpdesk-backend/src/main/java/br/com/bennytech/helpdesk/services/TecnicoService.java
@@ -3,6 +3,8 @@ package br.com.bennytech.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -42,6 +44,13 @@ public class TecnicoService {
 		return repository.save(newObj);
 	}
 
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		Tecnico oldObj = findById(id);
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Tecnico(objDTO);
+		return repository.save(oldObj);
+	}
 
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
@@ -55,6 +64,9 @@ public class TecnicoService {
 		}
 		
 	}
+
+
+	
 	
 	
 	
